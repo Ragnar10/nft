bb9325a808463bcea6d94fb695393b702d009e77
Гудов Олег Русланович <oleg.gudov@privatbank.ua>
2022-04-19 18:34:42 +0300

    initial

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+// Styles
+import Styles from './styles.module.scss';
+// Components
+
+const Main = () => {
+    return (
+        <div className = { Styles.container }>
+
+        </div>
+    );
+};
+
+export default Main;
