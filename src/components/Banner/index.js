c9e715ca59b74670fa7b1061baee19dd065f6750
mashka <marija.belousova@gmail.com>
2022-05-02 08:37:26 +0300

    create Banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,5 +1,7 @@
 // Styles
 import Styles from './styles.module.scss';
+//Images
+import openSea from '../../theme/assets/icons/opensea_logo.svg';
 
 
 const Banner = () => {
@@ -7,13 +9,21 @@ const Banner = () => {
         <section className = { Styles.banner }>
             <div className = { Styles.container }>
                 <div className = { Styles.banner_wrap }>
-                    <h2>TORNADO</h2>
-                    <p>EXPRESS CARD</p>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <h2 className = { Styles.caption }>TORNADO</h2>
+                    <p className = { Styles.title }>EXPRESS CARD</p>
+                    <p className = { Styles.info }>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     <div className = { Styles.btn_wrap }>
-
+                        <button className = { Styles.btn }>Mint</button>
+                        <button className = { Styles.btn_opensea }>
+                            <img src = { openSea } alt="open sea icon"/>
+                        </button>
                     </div>
                 </div>
+            </div>
+            <div className = {Styles.line}>
+                <span className = { Styles.line_text }>
+                    TORNADO EXPRESS CARD - Ron Bielecki - Exclusive Membership - TORNADO EXPRESS CARD - Ron Bielecki - Exclusive Membership -
+                </span>
             </div>
         </section>
     );
