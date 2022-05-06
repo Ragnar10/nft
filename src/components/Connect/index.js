e3d259a70eb134603aa0af7b41c4ba3819f5c36b
mashka <marija.belousova@gmail.com>
2022-05-06 20:10:40 +0300

    add adaptive to Connect

--- src/components/Connect/index.js
+++ src/components/Connect/index.js
@@ -11,14 +11,17 @@ const Connect = () => {
     return (
         <section className = { Styles.connect }>
             <div className = { Styles.container }>
-                <div className = { Styles.img_wrap }>
-                    <img src = { logo } alt = 'image' />
-                </div>
+                {/*<div className = { Styles.img_wrap }>*/}
+                {/*    <img src = { logo } alt = 'image' />*/}
+                {/*</div>*/}
                 <div className = { Styles.connect_wrap }>
                     <div>
                         <h2 className = { Styles.caption }>{ 'Connect' }</h2>
                         <h2 className = { Styles.title }>{ 'TORNADO EXPRESS CARD' }</h2>
                         <p className = { Styles.info }>{ 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ' }</p>
+                        <div className = { Styles.img_wrap }>
+                            <img src = { logo } alt = 'image' />
+                        </div>
                         <button className = { Styles.btn }>Connect</button>
                     </div>
                 </div>
