a77803cbb94cf3f8821a2a32e3580084169c390b
Гудов Олег Русланович <oleg.gudov@privatbank.ua>
2022-05-25 02:44:14 +0300

    add local to What Expect

--- src/components/WhatExpect/index.js
+++ src/components/WhatExpect/index.js
@@ -4,43 +4,47 @@ import { useTranslation } from 'react-i18next';
 import Styles from './styles.module.scss';
 
 const ListItem = ({ info }) => {
+    const setClass = (number) => {
+        if (number === '8') return Styles.item_img_infinity;
+        if (number === '64') return Styles.item_img_diskette;
+    };
+
     return (
         <div className = { Styles.list_item }>
-            <span>{ info.number }</span>
+            <span className = { setClass(info.number) }>{ info.number !== '8' && info.number !== '64' ? info.number : '' }</span>
             <h3>{ info.title }</h3>
-            <p>{ info.descr }</p>
+            { info.descr && <p>{ info.descr }</p> }
         </div>
     );
 };
 
-const list = [
-    {
-        id: 1, number: '4', title: 'Major Events', descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
-    },
-    {
-        id: 2, number: '12', title: 'Surprise Parties', descr: 'Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut',
-    },
-    {
-        id: 3, number: '14', title: 'Exclusive Raffles', descr: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
-    },
-    {
-        id: 4, number: '8', title: 'Exepteur sint est', descr: 'Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut',
-    },
-    {
-        id: 5, number: '7', title: 'Lorem Ipsum', descr: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
-    },
-];
-
 const WhatExpect = () => {
     const { t } = useTranslation();
+
+    const list = [
+        {
+            id: 1, number: '4', title: t('what_expect_major_title'), descr: t('what_expect_major_descr'),
+        },
+        {
+            id: 2, number: '6+', title: t('what_expect_surprise_title'), descr: t('what_expect_surprise_descr'),
+        },
+        {
+            id: 3, number: '8', title: t('what_expect_raffels_title'), descr: t('what_expect_raffels_descr'),
+        },
+        {
+            id: 4, number: '64', title: t('what_expect_content_title'), descr: t('what_expect_content_descr'),
+        },
+        {
+            id: 5, number: '250+', title: t('what_expect_other_title'),
+        },
+    ];
 
     return (
         <section className = { Styles.what_expect }>
             <div className = { Styles.container }>
                 <div className = { Styles.description }>
                     <div className = { Styles.description_caption }>
-                        <h2 className = { Styles.caption }>{ 'What to expect' }</h2>
-                        <p className = { Styles.info }>{ 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' }</p>
+                        <h2 className = { Styles.caption }>{ t('what_expect_title') }</h2>
                     </div>
                     <div className = { Styles.description_list }>
                         <div>
