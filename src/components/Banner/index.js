3d5b63a09a421ac532b9303c37f5669798dd0237
Гудов Олег Русланович <oleg.gudov@privatbank.ua>
2022-05-25 14:33:39 +0300

    add local to Banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -29,9 +29,9 @@ const Banner = () => {
                     classList = { Styles.socials_list } />
                 <div className = { Styles.container }>
                     <div className = { Styles.banner_wrap }>
-                        <h2 className = { Styles.caption }>{ 'TORNADO' }</h2>
-                        <p className = { Styles.title }>{ 'EXPRESS CARD' }</p>
-                        <p className = { Styles.info }>{ 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' }</p>
+                        <h2 className = { Styles.caption }>{ t('banner_title') }</h2>
+                        <p className = { Styles.title }>{ 'TORNADO CARD' }</p>
+                        <p className = { Styles.info }>{ t('banner_info') }</p>
                         <div className = { Styles.btn_wrap }>
                             <button
                                 onClick = { () => onToggle() }
@@ -45,20 +45,30 @@ const Banner = () => {
                 <div className = { Styles.line_wrap }>
                     <div className = { Styles.line_thin }>
                         <div>
-                            <span>{ 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' }</span>
-                            <span>{ 'incididunt ut labore et dolore magna aliqua. ' }</span>
-                            <span>{ 'Ut enim ad minim veniam, ' }</span>
-                            <span>{ 'quis nostrud exercitation ' }</span>
-                            <span>{ 'ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' }</span>
+                            <span>{ t('banner_ribbon_start') }</span>
+                            <span>{ t('banner_ribbon_middle') }</span>
+                            <span>{ `${t('banner_ribbon_end')} ${t('banner_ribbon_start')}` }</span>
+                            <span>{ t('banner_ribbon_middle') }</span>
+                            <span>{ `${t('banner_ribbon_end')} ${t('banner_ribbon_start')}` }</span>
+                            <span>{ t('banner_ribbon_middle') }</span>
+                            <span>{ `${t('banner_ribbon_end')} ${t('banner_ribbon_start')}` }</span>
+                            <span>{ t('banner_ribbon_middle') }</span>
+                            <span>{ `${t('banner_ribbon_end')} ${t('banner_ribbon_start')}` }</span>
+                            <span>{ t('banner_ribbon_middle') }</span>
+                            <span>{ `${t('banner_ribbon_end')} ${t('banner_ribbon_start')}` }</span>
+                            <span>{ t('banner_ribbon_middle') }</span>
+                            <span>{ t('banner_ribbon_end') }</span>
                         </div>
                     </div>
                     <div className = { Styles.line }>
                         <div>
-                            <span>{ 'TORNADO EXPRESS CARD - ' }</span>
-                            <span>{ 'Ron Bielecki ' }</span>
-                            <span>{ '- Exclusive Membership - TORNADO EXPRESS CARD - ' }</span>
-                            <span>{ 'Ron Bielecki ' }</span>
-                            <span>{ '- Exclusive Membership - ' }</span>
+                            <span>{ t('banner_ribbon_start') }</span>
+                            <span>{ t('banner_ribbon_middle') }</span>
+                            <span>{ `${t('banner_ribbon_end')} ${t('banner_ribbon_start')}` }</span>
+                            <span>{ t('banner_ribbon_middle') }</span>
+                            <span>{ `${t('banner_ribbon_end')} ${t('banner_ribbon_start')}` }</span>
+                            <span>{ t('banner_ribbon_middle') }</span>
+                            <span>{ t('banner_ribbon_end') }</span>
                         </div>
                     </div>
                 </div>
