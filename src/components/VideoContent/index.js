2ecbf58606ba41bb1b8bd42ac958fef278484c23
Гудов Олег Русланович <oleg.gudov@privatbank.ua>
2022-06-01 01:48:33 +0300

    video content hide

--- src/components/VideoContent/index.js
+++ src/components/VideoContent/index.js
@@ -12,11 +12,15 @@ const VideoContent = () => {
                 <h2 className = { Styles.caption }>{ t('video_content_title') }</h2>
                 <div className = { Styles.video_wrap }>
                     <div className = { Styles.video_ratio } />
-                    <iframe
-                        className = { Styles.video }
-                        src = { t('video_content_link') }
-                        allowFullScreen
-                        title = 'tornado video' />
+                    { /* <iframe */ }
+                    { /*    className = { Styles.video } */ }
+                    { /*    src = { t('video_content_link') } */ }
+                    { /*    allowFullScreen */ }
+                    { /*    title = 'tornado video' /> */ }
+                    <div className = { `${Styles.video} ${Styles.skeleton}` }>
+                        <div />
+                        <span>{ 'Coming soon...' }</span>
+                    </div>
                 </div>
             </div>
             <div className = { Styles.bg_texture } />
